3cbfa0d6f4d20dc7f785f5d3e9bf90757530f765
ZarNizza <gormaster@mail.ru>
2021-09-14 23:35:16 +0400

    inside promise OK, now need to get result out

--- src/GitPromise.js
+++ src/GitPromise.js
@@ -1,34 +1,48 @@
 // import { useState } from "react";
-import './App.css';
+import "./App.css";
 
 export function GitPromise() {
   let names = ["ZarNizza"];
-
+let rl = [];
   let requests = names.map((name) =>
     fetch(`https://api.github.com/users/${name}/repos`)
   );
 
-  let repoList = Promise.all(requests)
-    .then((responses) => {
-      // все промисы успешно завершены
-      for (let response of responses) {
-        console.log(`${response.url}: ${response.status}`); // покажет 200 для каждой ссылки
-      }
+  function getRepoList(requests) {
+    Promise.all(requests)
+      .then((responses) => {
+        for (let response of responses) {
+          console.log(`${response.url}: ${response.status}`); // покажет 200 для каждой ссылки
+        }
+        return responses;
+      })
+      .then((responses) => Promise.allSettled(responses.map((r) => r.json())))
+      .then(function rItem(repoArrs) {
+        let repoItems = [];
+        repoArrs.forEach((repoArr) =>
+          repoArr.value.map((repo) =>
+            repoItems.push(JSON.stringify(repo.full_name))
+          )
+        );
+        alert(repoItems);
+        rl = repoItems;
+        return repoItems;
+      })
+      .catch(alert);
+  }
 
-      return responses;
-    })
-    // преобразовать массив ответов response в response.json(),
-    // чтобы прочитать содержимое каждого
-    .then((responses) => Promise.allSettled(responses.map((r) => r.json())))
-    // все JSON-ответы обработаны, users - массив с результатами
-    .then((repoArrs) => repoArrs.forEach((repoArr) => alert("Repo: " + JSON.stringify(repoArr.value[0].name))))
-    .catch(alert);
-// ------------------------------------- WTF?????? need to extract repos from Promise/JSON 
-  console.log("repoList", repoList);
+  let repoList = getRepoList(requests);
 
   return (
     <div className="flexOuter">
-      <div className="flexInner">Good!</div>
+      <div className="flexInner">
+        Good!
+        <br />
+        {repoList}
+        <br />- - - - -<br/>
+        {rl}
+
+      </div>
     </div>
   );
 }
