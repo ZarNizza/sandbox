d88a9a216017a48014108756b1e5d1eabb3a26e7
ZarNizza <gormaster@mail.ru>
2021-09-15 21:37:35 +0400

    Good, with Ref

--- src/GitPromise.js
+++ src/GitPromise.js
@@ -2,30 +2,30 @@ import { useState } from "react";
 import { useRef } from "react";
 import "./App.css";
 import { getRepos } from "./getRepos";
-import react from "react";
 
 export function GitPromise() {
   const buttonRef = useRef(123);
   const [repos, setRepos] = useState([]);
-  const [err, setErr] = useState(false);
+  const [errMessage, setErrMessage] = useState(null);
   function showRepos() {
     const reposPromise = getRepos(buttonRef.current.value);
     reposPromise.then((repos) => setRepos(repos))
-    .catch(()=>setErr(true));
+    .catch((err) => setErrMessage(err.message));
   }
 
   return (
+    // при User=Undefined получаем и сообщение и ошибку одновременно
     <div>
       <input ref={buttonRef} />
       <button onClick={showRepos}>Show Repos</button>
-      {repos.length > 0 && (
+      {repos.length > 0 ? (
         <ul>
           {repos.map((repo) => (
-            <li key={repo}>{repo}</li>
+            <li key={repo.name}>{repo.name}</li>
           ))}
         </ul>
-      )}
-      {err && <p>ERROR!!!</p>}
+      ) : <p>{`User "${buttonRef.current.value}" has no public Repos`}</p>} 
+      {errMessage && <p>{errMessage}</p>}
     </div>
   );
 }
--- src/getRepos.js
+++ src/getRepos.js
@@ -1,9 +1,17 @@
 export function getRepos(username) {
   const gitURL = `https://api.github.com/users/${username}/repos`;
 
-return fetch(gitURL)
-.then(res=>res.json())
-.then(jsonRes => jsonRes.map(repoDetails=> repoDetails.full_name)
-    )
-
+  return fetch(gitURL)
+    .then((res) => {
+      console.log("res: ", res);
+      if (!res.ok) throw new Error("!!! Fetch NOT OK !!!");
+      res.json();
+    })
+    .then((jsonRes) => {
+      if (!Array.isArray(jsonRes)) return [];
+      jsonRes.map((repoDetails) => ({
+        name: repoDetails.full_name,
+        url: repoDetails.html_url,
+      }));
+    });
 }
