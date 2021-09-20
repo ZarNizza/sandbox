eaf1f554d21466aaf4f638214d8c0edff929bd0f
ZarNizza <gormaster@mail.ru>
2021-09-20 13:54:39 +0400

    useRef playground

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import {App} from './App';
 import {GitPromise} from './GitPromise';
+import { UseRefTest } from './UseRefTest';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <GitPromise />
+    {/* <GitPromise /> */}
+    <UseRefTest />
   </React.StrictMode>,
   document.getElementById('root')
 );
