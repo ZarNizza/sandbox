c1a01726d499895b0adf5efae9185b2674336a49
ZarNizza <gormaster@mail.ru>
2021-09-20 19:23:21 +0400

    State (Func and Class) - OK

--- src/UseEffectTest.js
+++ src/UseEffectTest.js
@@ -2,20 +2,22 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 export function FunctionUseState() {
-    const [c, sC] = useState(0);
-    useEffect(() => {
-      setTimeout(() => console.log("Func: count = ", c), 3000);
-      document.title = "C = " + c;
-    });
-    return (
-      <>
-        <p>Function Count = {c}</p>
-        <button onClick={() => sC(c + 1)}>Inc (+)</button>
-        <p>useState in Function updates with local environment Value, actual (fixed) at moment of function Call</p>
-        <p>- - - - - - - - - - -</p>
-      </>
-    );
-
+  const [c, sC] = useState(0);
+  useEffect(() => {
+    setTimeout(() => console.log("Func: count = ", c), 3000);
+    document.title = "C = " + c;
+  });
+  return (
+    <>
+      <p>Function Count = {c}</p>
+      <button onClick={() => sC(c + 1)}>Inc (+)</button>
+      <p>
+        useState in Function updates with local environment Value,<br/>
+        actual (fixed) at moment of function Call
+      </p>
+      <p>- - - - - - - - - - -</p>
+    </>
+  );
 }
 
 export class ClassThisState extends React.Component {
@@ -36,11 +38,18 @@ export class ClassThisState extends React.Component {
       );
     }, 3000);
   }
-  clickHandler = () => {this.setState((prevState, prevProps) => {return {count: ++prevState.count}})};
+  clickHandler = () => {
+    this.setState((prevState) => {
+      return { count: ++prevState.count };
+    });
+  };
   render() {
     return (
       <>
-      <p>this.state in Class update at the end of render cycle with last Value</p>
+        <p>
+          this.state in Class update at the end of render cycle with last Value<br/>
+          ! see ConsoleLog !
+        </p>
         <p>Class Count = {this.state.count}</p>
         <button ref={this.myRef} onClick={this.clickHandler}>
           abc
