23874a51178ad8a7703817c80e4598910b6b514d
ZarNizza <gormaster@mail.ru>
2021-10-09 10:26:10 +0400

    style

--- src/ABC.js
+++ src/ABC.js
@@ -98,80 +98,87 @@ export function RunCalc() {
   });
 
   return (
-    <table className="runCalc" >
+    <table className="runCalc">
       <caption>- Run Calc -</caption>
       <tbody>
-      <tr>
-        <td>м/с: </td>
-        <td>
-          <input
-            id="mSpeed"
-            value={state.mSpeed}
-            onChange={(event) => {
-              dispatch({ type: "mSpeed", value: event.target.value });
-            }}
-            type="text"
-            maxlength="4"
-          />{" "}
-        </td>
-      </tr>
-      <tr>
-        <td>км/ч: </td>
-        <td>
-          <input
-            id="kSpeed"
-            value={state.kSpeed}
-            onChange={(event) => {
-              dispatch({ type: "kSpeed", value: event.target.value });
-            }}
-            type="text"
-            maxlength="4"
-          />{" "}
-        </td>
-      </tr>
-      <tr>
-        <td>темп: </td>
-        <td>
-          <input
-            id="pace"
-            value={state.pace}
-            onChange={(event) => {
-              dispatch({ type: "pace", value: event.target.value });
-            }}
-            type="text"
-            maxlength="4"
-          />{" "}
-        </td>
-      </tr>
-      <tr><td> </td></tr>
-      <tr>
-        <td>каденс: </td>
-        <td>
-          <input
-            id="cadense"
-            value={state.cadense}
-            onChange={(event) => {
-              dispatch({ type: "cadense", value: event.target.value });
-            }}
-            type="text"
-            maxlength="4"
-          />{" "}
-        </td>
-      </tr>
-      <tr>
-        <td>длШага: </td>
-        <td>
-          <input
-            id="strLength"
-            value={state.strLength}
-            onChange={(event) => {
-              dispatch({ type: "strLength", value: event.target.value });
-            }}
-            type="text"
-            maxlength="4"
-          />{" "}
-        </td>
-      </tr>
+        <tr>
+          <td>м/с: </td>
+          <td>
+            <input
+              id="mSpeed"
+              value={state.mSpeed}
+              onChange={(event) => {
+                dispatch({ type: "mSpeed", value: event.target.value });
+              }}
+              type="text"
+              maxlength="4"
+            />{" "}
+          </td>
+          <td>м/с</td>
+        </tr>
+        <tr>
+          <td>км/ч: </td>
+          <td>
+            <input
+              id="kSpeed"
+              value={state.kSpeed}
+              onChange={(event) => {
+                dispatch({ type: "kSpeed", value: event.target.value });
+              }}
+              type="text"
+              maxlength="4"
+            />{" "}
+          </td>
+          <td>км/ч</td>
+        </tr>
+        <tr>
+          <td>темп: </td>
+          <td>
+            <input
+              id="pace"
+              value={state.pace}
+              onChange={(event) => {
+                dispatch({ type: "pace", value: event.target.value });
+              }}
+              type="text"
+              maxlength="4"
+            />{" "}
+          </td>
+          <td>мин/км</td>
+        </tr>
+        <tr>
+          <td> </td>
+        </tr>
+        <tr>
+          <td>каденс: </td>
+          <td>
+            <input
+              id="cadense"
+              value={state.cadense}
+              onChange={(event) => {
+                dispatch({ type: "cadense", value: event.target.value });
+              }}
+              type="text"
+              maxlength="4"
+            />{" "}
+          </td>
+          <td>шаг/мин</td>
+        </tr>
+        <tr>
+          <td>длШага: </td>
+          <td>
+            <input
+              id="strLength"
+              value={state.strLength}
+              onChange={(event) => {
+                dispatch({ type: "strLength", value: event.target.value });
+              }}
+              type="text"
+              maxlength="4"
+            />{" "}
+          </td>
+          <td>м</td>
+        </tr>
       </tbody>
     </table>
   );
