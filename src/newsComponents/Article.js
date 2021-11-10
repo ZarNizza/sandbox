1830380bf9b51f65e3424d28916ed806ce1d8c9c
ZarNizza <gormaster@mail.ru>
2021-11-10 13:54:43 +0400

    fetch from localhost is OK, list is OK

--- src/newsComponents/Article.js
+++ src/newsComponents/Article.js
@@ -1,9 +1,9 @@
 
-export function Article(props) {
-console.log("ARTICLE:", props.data);
-  const { author, text, bigText } = props.data;
+export function Article(key, data) {
+console.log("ARTICLE:", data);
+  const { author, text, bigText } = data;
   return (
-    <li key={props.key} className="article">
+    <li key={key} className="article">
       <p className="news__author">Auth: {author}</p>
       <p className="news__text">Text: {text}</p>
       <p className="news__big-text">bTxt: {bigText}</p>
